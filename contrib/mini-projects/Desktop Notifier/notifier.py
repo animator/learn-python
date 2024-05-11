d7210bed9907d60e18d2c0a277a7066113d50b14
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-05-12 00:13:45 +0530

    Create notifier.py

---
+++ contrib/mini-projects/Desktop Notifier/notifier.py
@@ -0,0 +1,5 @@
+# importing required modules and libraries  
+import datetime # to read present date  
+import time # to suspend the execution for a specific time  
+import requests # to retrieve COVID stats from web  
+from plyer import notification # to get notification on the computer  
