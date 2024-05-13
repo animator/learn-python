98f1d8631ea73b62d143e25ccfa69e79c083645d
Yatharth <yatharth.thakare211@vit.edu>
2024-05-13 12:43:14 +0530

    extract header from json

--- contrib/mini-projects/Convert_JSON_to_CSV/convert.py
+++ contrib/mini-projects/Convert_JSON_to_CSV/convert.py
@@ -3,12 +3,17 @@ import json
 if __name__ == '__main__':
     try:
         with open('input.json', 'r') as f:
-            data = json.loads(f.read())
+            data = json.load(f)
 
-        output = ','.join([*data[0]])
+        # Extracting headers dynamically from the keys of the first object
+        header = ','.join(data[0].keys())
+
+        # Creating CSV content
+        output = header
         for obj in data:
-            output += f'\n{obj["Name"]},{obj["age"]},{obj["birthyear"]}'
+            output += f'\n{",".join(str(obj[key]) for key in obj)}'
 
+        # Writing to output CSV file
         with open('output.csv', 'w') as f:
             f.write(output)
     except Exception as ex:
